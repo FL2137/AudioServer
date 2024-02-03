2bb0ebe618a6768602fe20c82b819ea381c42d16
FL2137 <franek1519@gmail.com>
2024-02-03 18:16:59 +0100

    Basic request handling via json

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include "AudioServer.hpp"
+#include "Request.hpp"
 
 int main()
 {
     AudioServer audioServer;
 
-
-    TcpServer::asyncServer("192.168.1.109", PORT, [](std::string request, std::string& response) {
+    TcpServer::asyncServer("192.168.1.109", PORT, [&audioServer](std::string request, std::string& response) {
         std::cout << "Request: " << request << " of size: " << request.size() << std::endl;
-
-        if (request == "CREATEROOM") {
-
-            audioServer.
+        json jsRequest = request;
 
 
-
+        if (jsRequest["type"] == "CREATEROOM") {
+            if (audioServer.createRoom(jsRequest["uid"].get<int>())) {
+                response = "OK";
+            }
+            else {
+                response = audioServer.lastError;
+            }
             return;
         }
         else {
-            response = "dzialaez";
             return;
         }
 
-        response = "ehh";
-        });
+    });
 }
