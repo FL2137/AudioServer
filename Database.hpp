85706b7ed6f47e3ae09946b6108e0c6076eba9b6
FL2137 <franek1519@gmail.com>
2024-03-01 20:45:45 +0100

    Tinkering with 3rd party in c++ is pure fun!

--- Database.hpp
+++ Database.hpp
@@ -1,15 +1,50 @@
 #pragma once
-
+#include <pgfe/pgfe.hpp>
+#include <fstream>
 #include <string>
+#include <iostream>
+#include <libpq-fe.h>
+using namespace dmitigr;
 
 class Database {
 
+public:
+
+	Database() {
+		pgfe::Connection_options options;
+		pgfe::Connection con;
+
+	}
+
+
+	//void connection() {
+	//	//setting connection options
+	//	pgfe::Connection_options options;
+
+	//	std::ifstream file("dbCreds.txt");
+
+	//	std::string creds;
+	//	options.set(pgfe::Communication_mode::net);
+
+	//	std::getline(file, creds);
+	//	options.set_hostname(creds);
+
+	//	std::getline(file, creds);
+	//	options.set_database(creds);
+
+	//	std::getline(file, creds);
+	//	options.set_username(creds);
+
+	//	std::getline(file, creds);
+	//	options.set_password(creds);
 
 
 
-private:
+	//	//making the connection
+	//	pgfe::Connection connection(options);
 
-	const std::string databaseAddress = "";
+	//	connection.connect();
+	//}
 
 
 };
