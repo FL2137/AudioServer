3cde406b5d29dfddeb434d9bedf2dbb0a254b801
FL2137 <franek1519@gmail.com>
2024-03-05 23:18:54 +0100

    Most basic implementation

--- WebSocketServer.hpp
+++ WebSocketServer.hpp
@@ -1,3 +1,28 @@
 #pragma once
 
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
 
+#include <functional>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+
+class WebSocketServer {
+public:
+	WebSocketServer() {
+		endpoint.set_error_channels(websocketpp::log::elevel::all);
+		endpoint.set_access_channels(websocketpp::log::alevel::all ^ websocketpp::log::alevel::frame_payload);
+
+		endpoint.init_asio();
+	}
+
+	void run() {
+		endpoint.listen(3005);
+		endpoint.start_accept();
+		endpoint.run();
+	}
+
+private:
+	server endpoint;
+};
