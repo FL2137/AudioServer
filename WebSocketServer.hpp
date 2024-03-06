f2f7732394d98e48663dd59fecf10bdbb1172461
FL2137 <franek1519@gmail.com>
2024-03-06 21:55:54 +0100

    Websocketpp server works i guess

--- WebSocketServer.hpp
+++ WebSocketServer.hpp
@@ -13,13 +13,36 @@ public:
 	WebSocketServer() {
 		endpoint.set_error_channels(websocketpp::log::elevel::all);
 		endpoint.set_access_channels(websocketpp::log::alevel::all ^ websocketpp::log::alevel::frame_payload);
-
 		endpoint.init_asio();
+
+
+
+		endpoint.set_message_handler(
+			std::bind(
+				&WebSocketServer::readHandler,
+				this,
+				std::placeholders::_1,
+				std::placeholders::_2
+			)
+		);
+
+	}
+
+	void readHandler(websocketpp::connection_hdl hdl, server::message_ptr message) {
+		std::cout << message->get_payload();
 	}
 
 	void run() {
-		endpoint.listen(3005);
+
+		boost::system::error_code error;
+		endpoint.listen("192.168.1.109", "3005", error);
 		endpoint.start_accept();
+		if (endpoint.is_listening() && endpoint.is_server()) {
+
+			boost::system::error_code er;
+			auto ep = endpoint.get_local_endpoint(er);
+			std::cout << "Server listening on " << ep.address().to_string() + ":" << ep.port() << std::endl;
+		}
 		endpoint.run();
 	}
 
