0a28b1d787b07aceddd8b354ec0f491e7cdc5caf
FL2137 <franek1519@gmail.com>
2024-03-20 18:30:26 +0100

    StunRequest implementation

--- StunClient.hpp
+++ StunClient.hpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <stdint.h>
 #include <string>
+#include <random>
 
 class StunClient {
 
@@ -15,10 +16,26 @@ private:
 
 
 
-//baisc stun message formats
+//baisc STUN message formats
 #pragma pack(push, 1)
 	struct StunRequest {
+		StunRequest() {
 
+			//generate random transaction ID
+			
+			std::random_device device;
+			std::mt19937 generator(device);
+			std::uniform_int_distribution<unsigned char> distribution(0, 255);
+			
+			for (int i = 0; i < 12; i++) {
+				transactionID[i] = distribution(generator);
+			}
+		}
+
+		const int16_t messageType = htons(0x0001);
+		const int16_t messageLen = htons(0x0000);
+		const int32_t magicCookie = htonl(0x2112A442);
+		std::array<unsigned char, 12> transactionID = {0};
 	};
 #pragma pack(pop)
 
