239009c96da47025894a77a570d390ff6f428675
FL2137 <franek1519@gmail.com>
2024-03-20 18:24:14 +0100

    base of the class

---
+++ StunClient.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <array>
+#include <stdint.h>
+#include <string>
+
+class StunClient {
+
+
+private:
+
+	StunClient() {
+
+	}
+
+
+
+//baisc stun message formats
+#pragma pack(push, 1)
+	struct StunRequest {
+
+	};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+	struct StunResponse {
+
+	};
+#pragma pack(pop)
+
+
+
+};
